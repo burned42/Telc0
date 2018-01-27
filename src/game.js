f1d99d9a4548361f37d82e7d476eb7f00de6b1a3
Thomas Christlieb <thomaschristlieb@hotmail.com>
2018-01-27 17:43:57 +0100

    Bird fixes

--- src/game.js
+++ src/game.js
@@ -57,7 +57,7 @@ runningGame.prototype = {
             let fly = this.game.birds[this.game.birds.length-1].animations.add('fly');
             this.game.birds[this.game.birds.length-1].animations.play('fly', 30, true);
             let aktbird = this.game.add.sprite(this.game.camera.x + Math.floor(Math.random() * this.game.camera.width + 1), this.game.camera.y + Math.floor(Math.random() * this.game.camera.height + 1), 'bird');
-            aktbird.animations.add('fly');
+            let fly = aktbird.animations.add('fly');
             aktbird.animations.play('fly', 10, true);
             this.game.birds.push(aktbird);
         }
@@ -228,7 +228,7 @@ runningGame.prototype = {
             }
 
             // Change rotation sometimes
-            if (Math.random() > 0.97) {
+            if (Math.random() > 0.95) {
                 aktbird.rotation += 90;
 
                 if (aktbird.rotation === 360) {
