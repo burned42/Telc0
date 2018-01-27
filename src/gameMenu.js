7e82f7324501f5f8babcb08b04ca4032c40bc36f
mychecknix <michaelbernhardgrueb@web.de>
2018-01-27 13:21:00 +0100

    again more on start page

--- src/gameMenu.js
+++ src/gameMenu.js
@@ -4,9 +4,9 @@ let gameMenu = function (game) {
 gameMenu.prototype = {
     create: function () {
         this.game.add.button((this.game.width - 64) / 2, 200, 'play', this.playGame, this);
-        this.game.add.image((this.game.width - 128) / 2, 100, 'telc0Logo');
-        //this.game.add.text((this.game.width - 100) / 2, 100, 'Telc0', {font: '42px Arial', fill: '#ffffff'});
-        this.game.add.text((this.game.width - 150) / 2, 300, 'Credits', {font: '42px Arial', fill: '#ffffff'});
+        this.game.add.image((this.game.width - 128) / 2, 30, 'telc0Logo');
+        let creditsText = this.game.add.text(this.game.width / 2, 300, 'Credits', {font: '42px Arial', fill: '#ffffff'});
+        creditsText.anchor.setTo(0.5, 0);
         space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     },
     update: function () {
