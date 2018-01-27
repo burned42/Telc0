848bf111f411e28a0eccb8376a5d7abff36999fb
Bernd Stellwag <burned@zerties.org>
2018-01-27 23:01:19 +0100

    mark TODO comment as TODO

--- src/gameOver.js
+++ src/gameOver.js
@@ -17,7 +17,7 @@ gameOver.prototype = {
         this.game.add.tween(endGame).to( { emitY: 200 }, 4000, Phaser.Easing.Sinusoidal.InOut, true, 0, Number.MAX_VALUE, true);
 
         this.game.add.text(this.game.width * 0.2, 200, 'Game Over', {font: '42px Arial', fill: '#ffffff'});
-        //replace this with menu button
+        // TODO replace this with menu button
         this.game.add.button(this.game.width * 0.1, this.game.height * 0.6, 'buttonPlay', this.playGame, this);
 
         this.space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
