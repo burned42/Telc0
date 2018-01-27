cbc0373c8ea68923acc7671a53e9eb17a53f9f89
mychecknix <michaelbernhardgrueb@web.de>
2018-01-27 19:07:31 +0100

    AGAIN: move global vars to telc0.js; need to be done for more

--- src/telc0.js
+++ src/telc0.js
@@ -4,6 +4,14 @@ let cellSize = 128;
 let mapRows = 16;
 let mapCols = 16;
 
+let maxHeight = document.getElementById('container').scrollHeight;
+let maxWidth = document.getElementById('container').scrollWidth;
+let mapHeight = Math.floor(maxHeight / mapCols);
+let mapWidth = Math.floor(maxWidth / mapRows);
+
+let birdspeed = 2;
+let numofbirds = 6;
+
 let moneytext;
 let bar;
 let lastmaintenance;
@@ -13,16 +21,12 @@ let timenow;
 let money = 1000;
 let towercost = -200;
 let revenueHouse = 50;
-let maintenanceinterval = 10 * 1000;
+let maintenanceinterval = 10 * 1000; // secends * milliseconds
 let maintenancecost = -100;
 
 //main game functions for each game step
 window.onload = function () {
-    let maxHeight = document.getElementById('container').scrollHeight;
-    let maxWidth = document.getElementById('container').scrollWidth;
-    let mapHeight = Math.floor(maxHeight / mapCols);
-    let mapWidth = Math.floor(maxWidth / mapRows);
-    let game = new Phaser.Game(maxWidth, maxHeight, Phaser.CANVAS, 'game', null, false, false, null);
+    game = new Phaser.Game(mapWidth, mapHeight, Phaser.CANVAS, 'game', null, false, false, null);
     game.state.add('boot', gameBoot);
     game.state.add('preload', gamePreload);
     game.state.add('menu', gameMenu);
