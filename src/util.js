e426cfc072f73cea7f236a88aaed4d7e51752140
Bernd Stellwag <burned@zerties.org>
2018-01-27 22:10:36 +0100

    add comment to prevent future confusion

--- src/util.js
+++ src/util.js
@@ -1,4 +1,10 @@
-
+/**
+ * Get a random int between min (inclusive) and max (exclusive).
+ *
+ * @param min
+ * @param max
+ * @returns int
+ */
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
