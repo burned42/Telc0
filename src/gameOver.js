7a12b43bc29397f02e818b9691d2d83cb298c8b9
mychecknix <michaelbernhardgrueb@web.de>
2018-01-27 22:32:29 +0100

    change Game over, but still needs changes

--- src/gameOver.js
+++ src/gameOver.js
@@ -17,7 +17,7 @@ gameOver.prototype = {
         game.add.tween(endGame).to( { emitY: 200 }, 4000, Phaser.Easing.Sinusoidal.InOut, true, 0, Number.MAX_VALUE, true);
 
         // "replay" need picture for this
-        this.game.add.button(this.game.width * 0.1, this.game.height * 0.6, 'replay', this.playGame, this);
+        this.game.add.button(this.game.width * 0.1, this.game.height * 0.6, 'buttonPlay', this.playGame, this);
 
         this.space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     },
